cb59ab3c5b015b08a5ac11b1596666fae3e054b0
steffent1493 <steffent1493@gmail.com>
2021-04-19 11:59:57 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove(main);
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader);
+newHeader.id = 'victory'
+victory.innerHTML = "<h1>Todd is the champion"
